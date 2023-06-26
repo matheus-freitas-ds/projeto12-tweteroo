5e1cc8255f34c1e06558f3e02eb9542d650aba90
Matheus <matheusvmja@gmail.com>
2023-06-26 05:34:32 -0300

    unauthorized message

--- src/app.js
+++ src/app.js
@@ -10,21 +10,30 @@ const tweets = [];
 
 app.post ('/sign-up', (req, res) => {
     const {username, avatar} = req.body;
+
     users.push({username, avatar});
+
     res.send('OK');
 });
 
 app.post ('/tweets', (req, res) => {
     const {username, tweet} = req.body;
+
+    if (!username){
+        return res.send('UNAUTHORIZED');
+    };
+
     tweets.push({username, tweet});
+
     res.send('OK');
 });
 
 app.get ('/tweets', (req, res) => {
     if (tweets.length > 10){
-        const feed = tweets.slice(-10);
-        return res.send(feed);
+        const feedlimit = tweets.slice(-10);
+        return res.send(feedlimit);
     };
+
     res.send(tweets);
 });
 
