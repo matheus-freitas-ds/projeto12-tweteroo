2dd3e381e536a180e80cbca27c2eb920d9c8d6a1
Matheus <matheusvmja@gmail.com>
2023-06-26 08:27:09 -0300

    get feed done

--- src/app.js
+++ src/app.js
@@ -8,34 +8,39 @@ app.use(express.json());
 const users = [];
 const tweets = [];
 
-app.post ('/sign-up', (req, res) => {
-    const {username, avatar} = req.body;
+app.post('/sign-up', (req, res) => {
+    const { username, avatar } = req.body;
 
-    users.push({username, avatar});
+    users.push({ username, avatar });
 
     res.send('OK');
 });
 
-app.post ('/tweets', (req, res) => {
-    const {username, tweet} = req.body;
+app.post('/tweets', (req, res) => {
+    const { username, tweet } = req.body;
     const usernameError = users.find((user) => user.username === username);
 
-    if (!usernameError){
+    if (!usernameError) {
         return res.send('UNAUTHORIZED');
     };
 
-    tweets.push({username, tweet});
+    tweets.push({ username, tweet });
 
     res.send('OK');
 });
 
-app.get ('/tweets', (req, res) => {
-    if (tweets.length > 10){
-        const feedlimit = feed.slice(-10);
-        return res.send(feedlimit);
-    };
+app.get('/tweets', (req, res) => {
+    const feed = tweets.map((element) => {
+        const user = users.find((u) => u.username === element.username);
+        return { ...element, avatar : user.avatar }
+    });
 
-    res.send(tweets);
+    if (feed.length > 10){
+        const feedLimit = feed.slice(-10);
+        return res.send(feedLimit);
+    }
+
+    res.send(feed);
 });
 
 app.listen(5000, () => console.log("niceeee"));
