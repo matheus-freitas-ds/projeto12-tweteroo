bd38e1fdd0d00c365a89ae43a3f74224d2b76738
Matheus <matheusvmja@gmail.com>
2023-06-26 04:54:02 -0300

    return last 10 tweets

--- src/app.js
+++ src/app.js
@@ -11,16 +11,20 @@ const tweets = [];
 app.post ('/sign-up', (req, res) => {
     const {username, avatar} = req.body;
     users.push({username, avatar});
-    res.send('Ok');
+    res.send('OK');
 });
 
 app.post ('/tweets', (req, res) => {
     const {username, tweet} = req.body;
     tweets.push({username, tweet});
-    res.send('Ok');
+    res.send('OK');
 });
 
 app.get ('/tweets', (req, res) => {
+    if (tweets.length > 10){
+        const feed = tweets.slice(-10);
+        return res.send(feed);
+    };
     res.send(tweets);
 });
 
